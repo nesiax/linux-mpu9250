8ee89f11ad96fd1f0b44b90796875c97109eb247
Nestor Diaz <nestor@tiendalinux.com>
2021-07-21 14:36:54 -0500

    cosmetic changes

--- imu.c
+++ imu.c
@@ -4,21 +4,21 @@
 //
 //  Copyright (c) 2013 Pansenti, LLC
 //
-//  Permission is hereby granted, free of charge, to any person obtaining a copy of 
-//  this software and associated documentation files (the "Software"), to deal in 
-//  the Software without restriction, including without limitation the rights to use, 
-//  copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the 
-//  Software, and to permit persons to whom the Software is furnished to do so, 
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of
+//  this software and associated documentation files (the "Software"), to deal in
+//  the Software without restriction, including without limitation the rights to use,
+//  copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+//  Software, and to permit persons to whom the Software is furnished to do so,
 //  subject to the following conditions:
 //
-//  The above copyright notice and this permission notice shall be included in all 
+//  The above copyright notice and this permission notice shall be included in all
 //  copies or substantial portions of the Software.
 //
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
-//  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
-//  PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
-//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
-//  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+//  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+//  PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 //  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include <stdio.h>
@@ -46,288 +46,288 @@ int done;
 
 void usage(char *argv_0)
 {
-	printf("\nUsage: %s [options]\n", argv_0);
-	printf("  -b <i2c-bus>          The I2C bus number where the IMU is. The default is 1 to use /dev/i2c-1.\n");
-	printf("  -s <sample-rate>      The IMU sample rate in Hz. Range 2-50, default 10.\n");
-	printf("  -y <yaw-mix-factor>   Effect of mag yaw on fused yaw data.\n");
-	printf("                           0 = gyro only\n");
-	printf("                           1 = mag only\n");
-	printf("                           > 1 scaled mag adjustment of gyro data\n");
-	printf("                           The default is 4.\n");
-	printf("  -a <accelcal file>    Path to accelerometer calibration file. Default is ./accelcal.txt\n");
-	printf("  -m <magcal file>      Path to mag calibration file. Default is ./magcal.txt\n");
-	printf("  -v                    Verbose messages\n");
-	printf("  -h                    Show this help\n");
+    printf("\nUsage: %s [options]\n", argv_0);
+    printf("  -b <i2c-bus>          The I2C bus number where the IMU is. The default is 1 to use /dev/i2c-1.\n");
+    printf("  -s <sample-rate>      The IMU sample rate in Hz. Range 2-50, default 10.\n");
+    printf("  -y <yaw-mix-factor>   Effect of mag yaw on fused yaw data.\n");
+    printf("                           0 = gyro only\n");
+    printf("                           1 = mag only\n");
+    printf("                           > 1 scaled mag adjustment of gyro data\n");
+    printf("                           The default is 4.\n");
+    printf("  -a <accelcal file>    Path to accelerometer calibration file. Default is ./accelcal.txt\n");
+    printf("  -m <magcal file>      Path to mag calibration file. Default is ./magcal.txt\n");
+    printf("  -v                    Verbose messages\n");
+    printf("  -h                    Show this help\n");
 
-	printf("\nExample: %s -b3 -s20 -y10\n\n", argv_0);
-	
-	exit(1);
+    printf("\nExample: %s -b3 -s20 -y10\n\n", argv_0);
+
+    exit(1);
 }
 
 int main(int argc, char **argv)
 {
-	int opt, len;
-	int i2c_bus = DEFAULT_I2C_BUS;
-	int sample_rate = DEFAULT_SAMPLE_RATE_HZ;
-	int yaw_mix_factor = DEFAULT_YAW_MIX_FACTOR;
-	int verbose = 0;
-	char *mag_cal_file = NULL;
-	char *accel_cal_file = NULL;
+    int opt, len;
+    int i2c_bus = DEFAULT_I2C_BUS;
+    int sample_rate = DEFAULT_SAMPLE_RATE_HZ;
+    int yaw_mix_factor = DEFAULT_YAW_MIX_FACTOR;
+    int verbose = 0;
+    char *mag_cal_file = NULL;
+    char *accel_cal_file = NULL;
 
-	while ((opt = getopt(argc, argv, "b:s:y:a:m:vh")) != -1) {
-		switch (opt) {
-		case 'b':
-			i2c_bus = strtoul(optarg, NULL, 0);
-			
-			if (errno == EINVAL)
-				usage(argv[0]);
-			
-			if (i2c_bus < MIN_I2C_BUS || i2c_bus > MAX_I2C_BUS)
-				usage(argv[0]);
+    while ((opt = getopt(argc, argv, "b:s:y:a:m:vh")) != -1) {
+        switch (opt) {
+        case 'b':
+            i2c_bus = strtoul(optarg, NULL, 0);
 
-			break;
-		
-		case 's':
-			sample_rate = strtoul(optarg, NULL, 0);
-			
-			if (errno == EINVAL)
-				usage(argv[0]);
-			
-			if (sample_rate < MIN_SAMPLE_RATE || sample_rate > MAX_SAMPLE_RATE)
-				usage(argv[0]);
+            if (errno == EINVAL)
+                usage(argv[0]);
 
-			break;
+            if (i2c_bus < MIN_I2C_BUS || i2c_bus > MAX_I2C_BUS)
+                usage(argv[0]);
 
-		case 'y':
-			yaw_mix_factor = strtoul(optarg, NULL, 0);
-			
-			if (errno == EINVAL)
-				usage(argv[0]);
-			
-			if (yaw_mix_factor < 0 || yaw_mix_factor > 100)
-				usage(argv[0]);
+            break;
 
-			break;
+        case 's':
+            sample_rate = strtoul(optarg, NULL, 0);
 
-		case 'a':
-			len = 1 + strlen(optarg);
+            if (errno == EINVAL)
+                usage(argv[0]);
 
-			accel_cal_file = (char *)malloc(len);
+            if (sample_rate < MIN_SAMPLE_RATE || sample_rate > MAX_SAMPLE_RATE)
+                usage(argv[0]);
 
-			if (!accel_cal_file) {
-				perror("malloc");
-				exit(1);
-			}
+            break;
 
-			strcpy(accel_cal_file, optarg);
-			break;
+        case 'y':
+            yaw_mix_factor = strtoul(optarg, NULL, 0);
 
-		case 'm':
-			len = 1 + strlen(optarg);
+            if (errno == EINVAL)
+                usage(argv[0]);
 
-			mag_cal_file = (char *)malloc(len);
+            if (yaw_mix_factor < 0 || yaw_mix_factor > 100)
+                usage(argv[0]);
 
-			if (!mag_cal_file) {
-				perror("malloc");
-				exit(1);
-			}
+            break;
 
-			strcpy(mag_cal_file, optarg);
-			break;
+        case 'a':
+            len = 1 + strlen(optarg);
 
-		case 'v':
-			verbose = 1;
-			break;
+            accel_cal_file = (char *)malloc(len);
 
-		case 'h':
-		default:
-			usage(argv[0]);
-			break;
-		}
-	}
+            if (!accel_cal_file) {
+                perror("malloc");
+                exit(1);
+            }
 
-	register_sig_handler();
+            strcpy(accel_cal_file, optarg);
+            break;
 
-	mpu9250_set_debug(verbose);
+        case 'm':
+            len = 1 + strlen(optarg);
 
-	if (mpu9250_init(i2c_bus, sample_rate, yaw_mix_factor))
-		exit(1);
+            mag_cal_file = (char *)malloc(len);
 
-	set_cal(0, accel_cal_file);
-	set_cal(1, mag_cal_file);
+            if (!mag_cal_file) {
+                perror("malloc");
+                exit(1);
+            }
 
-	if (accel_cal_file)
-		free(accel_cal_file);
+            strcpy(mag_cal_file, optarg);
+            break;
 
-	if (mag_cal_file)
-		free(mag_cal_file);
+        case 'v':
+            verbose = 1;
+            break;
 
-	read_loop(sample_rate);
+        case 'h':
+        default:
+            usage(argv[0]);
+            break;
+        }
+    }
 
-	mpu9250_exit();
+    register_sig_handler();
 
-	return 0;
+    mpu9250_set_debug(verbose);
+
+    if (mpu9250_init(i2c_bus, sample_rate, yaw_mix_factor))
+        exit(1);
+
+    set_cal(0, accel_cal_file);
+    set_cal(1, mag_cal_file);
+
+    if (accel_cal_file)
+        free(accel_cal_file);
+
+    if (mag_cal_file)
+        free(mag_cal_file);
+
+    read_loop(sample_rate);
+
+    mpu9250_exit();
+
+    return 0;
 }
 
 void read_loop(unsigned int sample_rate)
 {
-	unsigned long loop_delay;
-	mpudata_t mpu;
+    unsigned long loop_delay;
+    mpudata_t mpu;
 
-	memset(&mpu, 0, sizeof(mpudata_t));
+    memset(&mpu, 0, sizeof(mpudata_t));
 
-	if (sample_rate == 0)
-		return;
+    if (sample_rate == 0)
+        return;
 
-	loop_delay = (1000 / sample_rate) - 2;
+    loop_delay = (1000 / sample_rate) - 2;
 
-	printf("\nEntering read loop (ctrl-c to exit)\n\n");
+    printf("\nEntering read loop (ctrl-c to exit)\n\n");
 
-	linux_delay_ms(loop_delay);
+    linux_delay_ms(loop_delay);
 
-	while (!done) {
-		if (mpu9250_read(&mpu) == 0) {
-			print_fused_euler_angles(&mpu);
-			// printf_fused_quaternions(&mpu);
-			// print_calibrated_accel(&mpu);
-			// print_calibrated_mag(&mpu);
-		}
+    while (!done) {
+        if (mpu9250_read(&mpu) == 0) {
+            print_fused_euler_angles(&mpu);
+            //print_fused_quaternions(&mpu);
+            //print_calibrated_accel(&mpu);
+            //print_calibrated_mag(&mpu);
+        }
 
-		linux_delay_ms(loop_delay);
-	}
+        linux_delay_ms(loop_delay);
+    }
 
-	printf("\n\n");
+    printf("\n\n");
 }
 
 void print_fused_euler_angles(mpudata_t *mpu)
 {
-	printf("\rX: %0.0f Y: %0.0f Z: %0.0f        ",
-			mpu->fusedEuler[VEC3_X] * RAD_TO_DEGREE, 
-			mpu->fusedEuler[VEC3_Y] * RAD_TO_DEGREE, 
-			mpu->fusedEuler[VEC3_Z] * RAD_TO_DEGREE);
+    printf("\rX: %0.0f Y: %0.0f Z: %0.0f        ",
+           mpu->fusedEuler[VEC3_X] * RAD_TO_DEGREE,
+           mpu->fusedEuler[VEC3_Y] * RAD_TO_DEGREE,
+           mpu->fusedEuler[VEC3_Z] * RAD_TO_DEGREE);
 
-	fflush(stdout);
+    fflush(stdout);
 }
 
 void print_fused_quaternions(mpudata_t *mpu)
 {
-	printf("\rW: %0.2f X: %0.2f Y: %0.2f Z: %0.2f        ",
-			mpu->fusedQuat[QUAT_W],
-			mpu->fusedQuat[QUAT_X],
-			mpu->fusedQuat[QUAT_Y],
-			mpu->fusedQuat[QUAT_Z]);
+    printf("\rW: %0.2f X: %0.2f Y: %0.2f Z: %0.2f        ",
+           mpu->fusedQuat[QUAT_W],
+           mpu->fusedQuat[QUAT_X],
+           mpu->fusedQuat[QUAT_Y],
+           mpu->fusedQuat[QUAT_Z]);
 
-	fflush(stdout);
+    fflush(stdout);
 }
 
 void print_calibrated_accel(mpudata_t *mpu)
 {
-	printf("\rX: %05d Y: %05d Z: %05d        ",
-			mpu->calibratedAccel[VEC3_X], 
-			mpu->calibratedAccel[VEC3_Y], 
-			mpu->calibratedAccel[VEC3_Z]);
+    printf("\rX: %05d Y: %05d Z: %05d        ",
+           mpu->calibratedAccel[VEC3_X],
+           mpu->calibratedAccel[VEC3_Y],
+           mpu->calibratedAccel[VEC3_Z]);
 
-	fflush(stdout);
+    fflush(stdout);
 }
 
 void print_calibrated_mag(mpudata_t *mpu)
 {
-	printf("\rX: %03d Y: %03d Z: %03d        ",
-			mpu->calibratedMag[VEC3_X], 
-			mpu->calibratedMag[VEC3_Y], 
-			mpu->calibratedMag[VEC3_Z]);
+    printf("\rX: %03d Y: %03d Z: %03d        ",
+           mpu->calibratedMag[VEC3_X],
+           mpu->calibratedMag[VEC3_Y],
+           mpu->calibratedMag[VEC3_Z]);
 
-	fflush(stdout);
+    fflush(stdout);
 }
 
 int set_cal(int mag, char *cal_file)
 {
-	int i;
-	FILE *f;
-	char buff[32];
-	long val[6];
-	caldata_t cal;
+    int i;
+    FILE *f;
+    char buff[32];
+    long val[6];
+    caldata_t cal;
 
-	if (cal_file) {
-		f = fopen(cal_file, "r");
-		
-		if (!f) {
-			perror("open(<cal-file>)");
-			return -1;
-		}
-	}
-	else {
-		if (mag) {
-			f = fopen("./magcal.txt", "r");
-		
-			if (!f) {
-				printf("Default magcal.txt not found\n");
-				return 0;
-			}
-		}
-		else {
-			f = fopen("./accelcal.txt", "r");
-		
-			if (!f) {
-				printf("Default accelcal.txt not found\n");
-				return 0;
-			}
-		}		
-	}
+    if (cal_file) {
+        f = fopen(cal_file, "r");
 
-	memset(buff, 0, sizeof(buff));
-	
-	for (i = 0; i < 6; i++) {
-		if (!fgets(buff, 20, f)) {
-			printf("Not enough lines in calibration file\n");
-			break;
-		}
+        if (!f) {
+            perror("open(<cal-file>)");
+            return -1;
+        }
+    }
+    else {
+        if (mag) {
+            f = fopen("./magcal.txt", "r");
 
-		val[i] = atoi(buff);
+            if (!f) {
+                printf("Default magcal.txt not found\n");
+                return 0;
+            }
+        }
+        else {
+            f = fopen("./accelcal.txt", "r");
 
-		if (val[i] == 0) {
-			printf("Invalid cal value: %s\n", buff);
-			break;
-		}
-	}
+            if (!f) {
+                printf("Default accelcal.txt not found\n");
+                return 0;
+            }
+        }
+    }
 
-	fclose(f);
+    memset(buff, 0, sizeof(buff));
 
-	if (i != 6) 
-		return -1;
+    for (i = 0; i < 6; i++) {
+        if (!fgets(buff, 20, f)) {
+            printf("Not enough lines in calibration file\n");
+            break;
+        }
 
-	cal.offset[0] = (short)((val[0] + val[1]) / 2);
-	cal.offset[1] = (short)((val[2] + val[3]) / 2);
-	cal.offset[2] = (short)((val[4] + val[5]) / 2);
+        val[i] = atoi(buff);
 
-	cal.range[0] = (short)(val[1] - cal.offset[0]);
-	cal.range[1] = (short)(val[3] - cal.offset[1]);
-	cal.range[2] = (short)(val[5] - cal.offset[2]);
-	
-	if (mag) 
-		mpu9250_set_mag_cal(&cal);
-	else 
-		mpu9250_set_accel_cal(&cal);
+        if (val[i] == 0) {
+            printf("Invalid cal value: %s\n", buff);
+            break;
+        }
+    }
 
-	return 0;
+    fclose(f);
+
+    if (i != 6)
+        return -1;
+
+    cal.offset[0] = (short)((val[0] + val[1]) / 2);
+    cal.offset[1] = (short)((val[2] + val[3]) / 2);
+    cal.offset[2] = (short)((val[4] + val[5]) / 2);
+
+    cal.range[0] = (short)(val[1] - cal.offset[0]);
+    cal.range[1] = (short)(val[3] - cal.offset[1]);
+    cal.range[2] = (short)(val[5] - cal.offset[2]);
+
+    if (mag)
+        mpu9250_set_mag_cal(&cal);
+    else
+        mpu9250_set_accel_cal(&cal);
+
+    return 0;
 }
 
 void register_sig_handler()
 {
-	struct sigaction sia;
+    struct sigaction sia;
 
-	bzero(&sia, sizeof sia);
-	sia.sa_handler = sigint_handler;
+    bzero(&sia, sizeof sia);
+    sia.sa_handler = sigint_handler;
 
-	if (sigaction(SIGINT, &sia, NULL) < 0) {
-		perror("sigaction(SIGINT)");
-		exit(1);
-	} 
+    if (sigaction(SIGINT, &sia, NULL) < 0) {
+        perror("sigaction(SIGINT)");
+        exit(1);
+    }
 }
 
 void sigint_handler(int sig)
 {
-	done = 1;
+    done = 1;
 }
 
 /* Local Variables:  */
--- imucal.c
+++ imucal.c
@@ -4,21 +4,21 @@
 //
 //  Copyright (c) 2013 Pansenti, LLC
 //
-//  Permission is hereby granted, free of charge, to any person obtaining a copy of 
-//  this software and associated documentation files (the "Software"), to deal in 
-//  the Software without restriction, including without limitation the rights to use, 
-//  copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the 
-//  Software, and to permit persons to whom the Software is furnished to do so, 
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of
+//  this software and associated documentation files (the "Software"), to deal in
+//  the Software without restriction, including without limitation the rights to use,
+//  copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+//  Software, and to permit persons to whom the Software is furnished to do so,
 //  subject to the following conditions:
 //
-//  The above copyright notice and this permission notice shall be included in all 
+//  The above copyright notice and this permission notice shall be included in all
 //  copies or substantial portions of the Software.
 //
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
-//  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
-//  PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
-//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
-//  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+//  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+//  PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 //  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include <stdio.h>
@@ -62,7 +62,7 @@ void usage(char *argv_0)
     printf("  -h                    Show this help\n");
 
     printf("\nExample: %s -b3 -s20 -a\n\n", argv_0);
-	
+
     exit(1);
 }
 
@@ -71,80 +71,80 @@ int main(int argc, char **argv)
     int opt;
     int i2c_bus = DEFAULT_I2C_BUS;
     int sample_rate = DEFAULT_SAMPLE_RATE_HZ;
-	
+
     mag_mode = -1;
 
     memset(calFile, 0, sizeof(calFile));
 
     while ((opt = getopt(argc, argv, "b:s:y:amh")) != -1) {
-	switch (opt) {
-	case 'b':
-	    i2c_bus = strtoul(optarg, NULL, 0);
-			
-	    if (errno == EINVAL)
-		usage(argv[0]);
-			
-	    if (i2c_bus < MIN_I2C_BUS || i2c_bus > MAX_I2C_BUS)
-		usage(argv[0]);
+        switch (opt) {
+        case 'b':
+            i2c_bus = strtoul(optarg, NULL, 0);
 
-	    break;
-		
-	case 's':
-	    sample_rate = strtoul(optarg, NULL, 0);
-			
-	    if (errno == EINVAL)
-		usage(argv[0]);
-			
-	    if (sample_rate < MIN_SAMPLE_RATE || sample_rate > MAX_SAMPLE_RATE)
-		usage(argv[0]);
+            if (errno == EINVAL)
+                usage(argv[0]);
 
-	    break;
+            if (i2c_bus < MIN_I2C_BUS || i2c_bus > MAX_I2C_BUS)
+                usage(argv[0]);
 
-	case 'y':
-	    if (strlen(optarg) >= sizeof(calFile)) {
-		printf("Choose a shorter path for the cal file\n");
-		usage(argv[0]);
-	    }
+            break;
 
-	    strcpy(calFile, optarg);
-	    break;
+        case 's':
+            sample_rate = strtoul(optarg, NULL, 0);
 
-	case 'a':
-	    if (mag_mode != -1)
-		usage(argv[0]);
+            if (errno == EINVAL)
+                usage(argv[0]);
 
-	    mag_mode = 0;
-	    break;
+            if (sample_rate < MIN_SAMPLE_RATE || sample_rate > MAX_SAMPLE_RATE)
+                usage(argv[0]);
 
-	case 'm':
-	    if (mag_mode != -1)
-		usage(argv[0]);
+            break;
 
-	    mag_mode = 1;
-	    break;
-		
-	case 'h':
-	default:
-	    usage(argv[0]);
-	    break;
-	}
+        case 'y':
+            if (strlen(optarg) >= sizeof(calFile)) {
+                printf("Choose a shorter path for the cal file\n");
+                usage(argv[0]);
+            }
+
+            strcpy(calFile, optarg);
+            break;
+
+        case 'a':
+            if (mag_mode != -1)
+                usage(argv[0]);
+
+            mag_mode = 0;
+            break;
+
+        case 'm':
+            if (mag_mode != -1)
+                usage(argv[0]);
+
+            mag_mode = 1;
+            break;
+
+        case 'h':
+        default:
+            usage(argv[0]);
+            break;
+        }
     }
 
     if (mag_mode == -1)
-	usage(argv[0]);
+        usage(argv[0]);
 
     register_sig_handler();
 
     if (mpu9250_init(i2c_bus, sample_rate, 0))
-	exit(1);
+        exit(1);
 
     read_loop(sample_rate);
 
     if (strlen(calFile) == 0) {
-	if (mag_mode)
-	    strcpy(calFile, "magcal.txt");
-	else
-	    strcpy(calFile, "accelcal.txt");
+        if (mag_mode)
+            strcpy(calFile, "magcal.txt");
+        else
+            strcpy(calFile, "accelcal.txt");
     }
 
     write_cal();
@@ -161,13 +161,13 @@ void read_loop(unsigned int sample_rate)
     mpudata_t mpu;
 
     if (sample_rate == 0)
-	return;
+        return;
 
     memset(&mpu, 0, sizeof(mpudata_t));
 
     for (i = 0; i < 3; i++) {
-	minVal[i] = 0x7fff;
-	maxVal[i] = 0x8000;
+        minVal[i] = 0x7fff;
+        maxVal[i] = 0x8000;
     }
 
     loop_delay = (1000 / sample_rate) - 2;
@@ -177,47 +177,47 @@ void read_loop(unsigned int sample_rate)
     linux_delay_ms(loop_delay);
 
     while (!done) {
-	change = 0;
+        change = 0;
 
-	if (mag_mode) {
-	    if (mpu9250_read_mag(&mpu) == 0) {
-		for (i = 0; i < 3; i++) {
-		    if (mpu.rawMag[i] < minVal[i]) {
-			minVal[i] = mpu.rawMag[i];
-			change = 1;
-		    }
-				
-		    if (mpu.rawMag[i] > maxVal[i]) {
-			maxVal[i] = mpu.rawMag[i];
-			change = 1;
-		    }
-		}
-	    }
-	}
-	else {
-	    if (mpu9250_read_dmp(&mpu) == 0) {
-		for (i = 0; i < 3; i++) {
-		    if (mpu.rawAccel[i] < minVal[i]) {
-			minVal[i] = mpu.rawAccel[i];
-			change = 1;
-		    }
-				
-		    if (mpu.rawAccel[i] > maxVal[i]) {
-			maxVal[i] = mpu.rawAccel[i];
-			change = 1;
-		    }
-		}
-	    }
-	}
-		
-	if (change) {
-	    if (mag_mode)
-		print_mag(&mpu);
-	    else
-		print_accel(&mpu);
-	}
+        if (mag_mode) {
+            if (mpu9250_read_mag(&mpu) == 0) {
+                for (i = 0; i < 3; i++) {
+                    if (mpu.rawMag[i] < minVal[i]) {
+                        minVal[i] = mpu.rawMag[i];
+                        change = 1;
+                    }
 
-	linux_delay_ms(loop_delay);
+                    if (mpu.rawMag[i] > maxVal[i]) {
+                        maxVal[i] = mpu.rawMag[i];
+                        change = 1;
+                    }
+                }
+            }
+        }
+        else {
+            if (mpu9250_read_dmp(&mpu) == 0) {
+                for (i = 0; i < 3; i++) {
+                    if (mpu.rawAccel[i] < minVal[i]) {
+                        minVal[i] = mpu.rawAccel[i];
+                        change = 1;
+                    }
+
+                    if (mpu.rawAccel[i] > maxVal[i]) {
+                        maxVal[i] = mpu.rawAccel[i];
+                        change = 1;
+                    }
+                }
+            }
+        }
+
+        if (change) {
+            if (mag_mode)
+                print_mag(&mpu);
+            else
+                print_accel(&mpu);
+        }
+
+        linux_delay_ms(loop_delay);
     }
 
     printf("\n\n");
@@ -226,9 +226,9 @@ void read_loop(unsigned int sample_rate)
 void print_accel(mpudata_t *mpu)
 {
     printf("\rX %d|%d|%d    Y %d|%d|%d    Z %d|%d|%d             ",
-	   minVal[0], mpu->rawAccel[0], maxVal[0], 
-	   minVal[1], mpu->rawAccel[1], maxVal[1],
-	   minVal[2], mpu->rawAccel[2], maxVal[2]);
+           minVal[0], mpu->rawAccel[0], maxVal[0],
+           minVal[1], mpu->rawAccel[1], maxVal[1],
+           minVal[2], mpu->rawAccel[2], maxVal[2]);
 
     fflush(stdout);
 }
@@ -236,9 +236,9 @@ void print_accel(mpudata_t *mpu)
 void print_mag(mpudata_t *mpu)
 {
     printf("\rX %d|%d|%d    Y %d|%d|%d    Z %d|%d|%d             ",
-	   minVal[0], mpu->rawMag[0], maxVal[0], 
-	   minVal[1], mpu->rawMag[1], maxVal[1],
-	   minVal[2], mpu->rawMag[2], maxVal[2]);
+           minVal[0], mpu->rawMag[0], maxVal[0],
+           minVal[1], mpu->rawMag[1], maxVal[1],
+           minVal[2], mpu->rawMag[2], maxVal[2]);
 
     fflush(stdout);
 }
@@ -251,13 +251,13 @@ void write_cal()
     fd = open(calFile, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 
     if (fd < 0) {
-	perror("open(calFile)");
-	return;
+        perror("open(calFile)");
+        return;
     }
 
     for (i = 0; i < 3; i++) {
-	sprintf(buff, "%d\n%d\n", minVal[i], maxVal[i]);
-	write(fd, buff, strlen(buff));
+        sprintf(buff, "%d\n%d\n", minVal[i], maxVal[i]);
+        write(fd, buff, strlen(buff));
     }
 
     close(fd);
@@ -271,9 +271,9 @@ void register_sig_handler()
     sia.sa_handler = sigint_handler;
 
     if (sigaction(SIGINT, &sia, NULL) < 0) {
-	perror("sigaction(SIGINT)");
-	exit(1);
-    } 
+        perror("sigaction(SIGINT)");
+        exit(1);
+    }
 }
 
 void sigint_handler(int sig)
